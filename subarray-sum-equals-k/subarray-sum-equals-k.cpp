1f2f85983e8a766f0123f4c365dfa66690ad6c27
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2021-09-06 14:00:13 +0530

    Time: 304 ms (5.05%), Space: 44.4 MB (7.44%) - LeetHub

--- subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,13 +2,17 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
       int currSum =0;
-      int count;
+      int count=0;
       map<int,int>storeMap;
       storeMap[0]++;
       for(auto i : nums){
         currSum+=i;
         count+=storeMap[currSum-k];
         storeMap[currSum]++;
+      }
+      //cout<<storeMap[1000];
+      for(auto i : storeMap){
+        cout<<i.first<<"    "<<i.second<<"  "<<endl;
       }
       return count;
     }
