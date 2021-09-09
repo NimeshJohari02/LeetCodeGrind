7ad7bd08a238d220e94a951012a9f7c4ecdfb349
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2021-09-09 19:14:58 +0530

    Time: 14 ms (37.70%), Space: 5.8 MB (91.11%) - LeetHub

--- palindrome-number/palindrome-number.cpp
+++ palindrome-number/palindrome-number.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string s = to_string(x);
-        string s2(s);
-         reverse(s.begin(),s.end());
-      return s==s2;
+      if(x<0){
+        return false;
+      }
+      int init=x;
+      long long int ans =0;
+      while(x){
+        ans=ans*10+(x%10);
+        x=x/10;
+      }
+      return ans==init;
     }
 };
