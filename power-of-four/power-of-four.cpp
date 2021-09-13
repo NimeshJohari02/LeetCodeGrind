4eb7a8c2cb9edaa732e941436aed355c97e76afc
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2021-09-13 19:06:57 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (24.02%) - LeetHub

---
+++ power-of-four/power-of-four.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n==0)
+            return false;
+        
+        return (ceil(log(n)/log(4))==floor(log(n)/log(4)));
+    }
+};
