ccbc0e7bb7d39b2d6724273e8712c92413aea101
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2021-09-20 15:44:03 +0530

    Time: 18 ms (6.73%), Space: 11 MB (5.05%) - LeetHub

---
+++ intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+         map<int,int>m1;
+       map<int,int>m2;
+      vector<int>ans;
+      for(auto it : nums1){
+        m1[it]++;
+      }
+      for(auto it : nums2){
+        m2[it]++;
+      }
+    for(auto it : m1 ){
+      if(m2[it.first]!=0){
+        // int k = min(m2[it.first],it.second);
+        // cout<<k<<endl;
+        // cout<<"it"<<it.first<<"    ->   "<<it.second<<endl;
+        ans.push_back(it.first);
+      }
+        }    
+      return ans;
+    }
+};
