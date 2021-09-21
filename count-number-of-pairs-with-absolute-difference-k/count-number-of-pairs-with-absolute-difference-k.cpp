a106ce0d743e23496ea8528aa6c99663fd337365
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2021-09-21 15:05:20 +0530

    Added Optimised Approach

--- count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.cpp
+++ count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.cpp
@@ -1,15 +1,28 @@
 class Solution {
-//O(n2)
 public:
     int countKDifference(vector<int>& nums, int k) {
+//O(n2)
+      //     int count =0;
+  //     for(int i=0;i<nums.size()-1;i++){
+  //   for(int j=i+1;j<nums.size();j++){
+  //     if(abs(nums[j]-nums[i])==k){
+  //       count++;
+  //     }
+  //   }
+  // }
+  //     return count;
+      //Optimised Approach;
+      map<int,int>mp;
       int count =0;
-      for(int i=0;i<nums.size()-1;i++){
-    for(int j=i+1;j<nums.size();j++){
-      if(abs(nums[j]-nums[i])==k){
-        count++;
+      for(auto it : nums ){
+        if(mp[it-k]){
+          count +=mp[it-k];
+        }
+        if(mp[it+k]){
+          count +=mp[it+k];
+        }
+        mp[it]++;
       }
-    }
-  }
-      return count;
+    return count ;
     }
 };
