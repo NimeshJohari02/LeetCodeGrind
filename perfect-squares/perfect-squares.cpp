4b5c34a0f4fe7a8faf6e5b4397727b5a59bab277
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2021-10-14 10:18:04 +0530

    Time: 3 ms (96.18%), Space: 5.9 MB (97.49%) - LeetHub

--- perfect-squares/perfect-squares.cpp
+++ perfect-squares/perfect-squares.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int dp[10001];
     int numSquares(int n ){
-        dp[0]=0;
+       /* dp[0]=0;
         vector<int>sq;
         dp[1]=1;
         sq.push_back(1);
@@ -22,6 +22,24 @@ public:
                 dp[i]=currMin;
             }
         }
-     return dp[n];   
+     return dp[n];   */
+        //The second approach is taken from the comments section it used 
+        //https://en.wikipedia.org/wiki/Lagrange's_identity_(disambiguation)
+            while (n % 4 == 0)
+        n /= 4;
+    if (n % 8 == 7)
+        return 4;
+    int a = -1, b = sqrt(n);
+    n -= b * b;
+    b += b + 1;
+    while (a <= b) {
+        if (n < 0)
+            n += b -= 2;
+        else if (n > 0)
+            n -= a += 2;
+        else
+            return a < 0 ? 1 : 2;
+    }
+    return 3;
     }
 };
