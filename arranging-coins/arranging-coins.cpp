3e4d0b68dfea21c05112410b761d6af98c5c0d0a
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2021-11-05 09:55:43 +0530

    Time: 12 ms (25.97%), Space: 5.9 MB (70.45%) - LeetHub

---
+++ arranging-coins/arranging-coins.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long long int i=1;
+        while((i*(i+1)/2)<=n){
+            i++;
+        }
+        return (i-1);
+        // return 0;
+    }
+};
