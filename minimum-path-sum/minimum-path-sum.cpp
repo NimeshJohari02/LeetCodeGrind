ca4e9a22fa1ba2b5e63940f4cf78c8abe72daedb
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2021-11-07 13:10:42 +0530

    Time: 24 ms (7.05%), Space: 10.5 MB (5.53%) - LeetHub

---
+++ minimum-path-sum/minimum-path-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+      int row = grid.size() ;
+        int col = grid[0].size();
+        vector<vector<int>>dp(row,vector<int>(col,0));
+        for(int i=0 ; i <row ; i++ ){
+            dp[i][0]=i==0?grid[i][0]:grid[i][0]+dp[i-1][0];
+        }
+          for(int j=0 ; j <col ; j++ ){
+            dp[0][j]=j==0?grid[0][j]:grid[0][j]+dp[0][j-1];
+        }
+        for(int i=1 ; i <row ; i++)
+            for(int j = 1 ; j < col ; j++){
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1])+grid[i][j]; 
+            }
+    for(auto it : dp){
+        for(auto i : it){
+            cout<<i<<"  ";
+        }
+        cout<<"\n";
+    }
+    return dp[row-1][col-1];
+    }
+};
