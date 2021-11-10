d4e53780a76999cc833bf60003cb76b9ffa3ed92
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2021-11-10 10:45:29 +0530

    Update best-time-to-buy-and-sell-stock-ii.cpp
    Added Explanation For future reference

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -3,6 +3,17 @@ public:
     
     int maxProfit(vector<int>& prices) {
         int buyIdx =0;
+        //  The idea is to buy at local min and dont sell till next element is larger than currPrice . This way 
+        /*
+           /s\      /s\    /(s)
+          /   \    /   \  /
+        b/     \b /     \b
+        even if you have something like this  you can buy and sell plus add the last case . ie after the end of the loop 
+        add 
+         currProfit+=(prices[sellIdx]-prices[buyIdx]);
+        because just in case there is no dip at the last just like above given figure then that part will not be included 
+        if there is a dip then the val would be zero hence no over counts .
+        */
         int sellIdx =0;
         int currProfit =0;
         for(int i =1 ; i < prices.size() ; i++){
