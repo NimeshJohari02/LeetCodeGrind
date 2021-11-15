e6f9ca67e3209be3099aba77a912bc0674973002
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2021-11-15 16:12:39 +0530

    Update find-the-kth-largest-integer-in-the-array.cpp

--- find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.cpp
+++ find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.cpp
@@ -13,3 +13,26 @@ public:
         return nums[nums.size()-k];
     }
 };
+// Another approach isto use Heap . But since the comparision logic is not a standard . We have to make a custom HeapCmp Class and in that class overload the operator ()
+// InOrder to facilitate the ordering of elements in the fashion that we want .
+class NumStrMinComparator {
+public:
+    bool operator() (string &b, string &a) { // Please note, I swap position of `a` and `b` here!
+        if (a.size() != b.size()) return a.size() < b.size();
+        return  a < b;
+    }
+};
+
+class Solution {
+public:
+    string kthLargestNumber(vector<string>& nums, int k) {
+        priority_queue<string, vector<string>, NumStrMinComparator> minHeap;
+        for (string& num : nums) {
+            minHeap.push(num);
+            if (minHeap.size() > k) {
+                minHeap.pop();
+            }
+        }
+        return minHeap.top();
+    }
+};
