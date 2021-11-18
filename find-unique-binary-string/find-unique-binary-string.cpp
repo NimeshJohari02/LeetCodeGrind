637bbd8dece87f76f48ecd7f63b605143bdceda3
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2021-11-18 20:51:09 +0530

    Update find-unique-binary-string.cpp

--- find-unique-binary-string/find-unique-binary-string.cpp
+++ find-unique-binary-string/find-unique-binary-string.cpp
@@ -41,3 +41,17 @@ public:
        return  getBinaryFromInt(sizeOfBStirng, ansInt);
     }
 };
+
+
+// Another Approach Based On Cantor's Diagonalization ....
+
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        string ans="";
+        for(int i=0; i<nums.size(); i++) 
+            ans+= nums[i][i]=='0' ? '1' : '0';          // Using ternary operator
+			// ans+=to_string(1-(nums[i][i]-'0'));     // Alternate:  or use to_string & 1-x to flip
+        return ans;
+    }
+};
