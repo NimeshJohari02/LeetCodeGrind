92f9aa36a095b80d7db92cf1400b2bc35024d814
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2021-11-23 13:08:39 +0530

    Time: 23 ms (32.21%), Space: 16.8 MB (66.13%) - LeetHub

---
+++ populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,51 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+// I need to make 2 sorts of connections Nodes With Same Parent  and Nodes with Different Parent 
+
+class Solution {
+public:
+    Node* connectWithSameParent(Node* root){
+        if(root == NULL){
+            return NULL;
+        }
+        if(root->left != NULL and root->right != NULL){
+            root->left->next = root->right;
+        }
+        root->left = connectWithSameParent(root->left);
+        root->right = connectWithSameParent(root->right);
+        return root;
+    }
+    Node* connectWithDifferentParent(Node *root){
+        if(root == NULL){
+            return NULL;
+        }
+        if(root->right != NULL and root->next != NULL){
+            root->right->next = root->next->left;
+        }
+        root->right =connectWithDifferentParent(root->right);
+        root->left =connectWithDifferentParent(root->left);
+        return root;
+    }
+    Node* connect(Node* root) {
+        if(root== NULL){
+            return NULL;
+        }
+        Node* modifiedRoot = connectWithSameParent(root);
+        return connectWithDifferentParent(modifiedRoot);
+    }
+};
