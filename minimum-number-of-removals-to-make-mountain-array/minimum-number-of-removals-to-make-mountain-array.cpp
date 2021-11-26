4593d205edf4262693a248e8a4395fe76d7b6027
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2021-11-26 11:31:23 +0530

    Update minimum-number-of-removals-to-make-mountain-array.cpp

--- minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
+++ minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.cpp
@@ -34,3 +34,38 @@ public:
     }
 };
 
+//Another Approach is to use binary search Along with LCS to enable a finer Time Complexity 
+
+
+
+class Solution {
+public:
+    int minimumMountainRemovals(vector<int>& nums) {
+        int n = nums.size(), m;
+        vector<int> lis, lds; lis.resize(n); lds.resize(n);
+        int *candidate = new int[n];
+        
+        lis[0] = 1; candidate[0] = nums[0]; m = 1;
+        // Reducing the Typical LIS from n2 to nlogn . As mountain is asked thus parts need to be increasing in order when looking uphill and decreasing when going reverse.
+        for(int i=1;i<n;i++) {
+            int j = lower_bound(candidate, candidate + m, nums[i]) - candidate;
+            candidate[j] = nums[i];
+            m = max(m, j+1);
+            lis[i] = j+1;
+        }
+        
+        lds[n-1] = 1; candidate[0] = nums[n-1]; m = 1;
+        for(int i=n-2;i>=0;i--) {
+            int j = lower_bound(candidate, candidate + m, nums[i]) - candidate;
+            candidate[j] = nums[i];
+            m = max(m, j+1);
+            lds[i] = j+1;
+        }
+        
+        m = 1;
+        for(int i=1;i<n-1;i++) if( lis[i]!=1 && lds[i]!=1 )
+            m = max(m, lis[i] + lds[i] - 1);
+        return n-m;
+    }
+};
+
