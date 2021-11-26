2e4170ca85c9df26cfd63e313be371497dbf29f9
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2021-11-26 14:18:34 +0530

    Update longest-increasing-subsequence (NlogN)

--- longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -20,3 +20,16 @@ public:
         return mn;
     }
 };
+
+
+// LIS NlogN solution better for future Problems 
+
+int lengthOfLIS(vector<int>& nums) {
+    vector<int> res;
+    for(int i=0; i<nums.size(); i++) {
+        auto it = std::lower_bound(res.begin(), res.end(), nums[i]);
+        if(it==res.end()) res.push_back(nums[i]);
+        else *it = nums[i];
+    }
+    return res.size();
+}
