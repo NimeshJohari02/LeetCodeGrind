b8ff2bf49a6f333e414553312c009621dc778b15
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2021-12-05 10:16:08 +0530

    Time: 28 ms (20.81%), Space: 22.2 MB (89.41%) - LeetHub

---
+++ house-robber-iii/house-robber-iii.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// Approach 1 Calculate Level Sum And Use House Robber 1 
+
+// This approach is based on finding grand children ll is left left and similarly finding any/all grand children 
+    class Solution {
+public:
+    int tryRob(TreeNode* root, int& l, int& r) {
+        if (!root)
+            return 0;
+            
+        int ll = 0, lr = 0, rl = 0, rr = 0;
+        l = tryRob(root->left, ll, lr);
+        r = tryRob(root->right, rl, rr);
+        
+        return max(root->val + ll + lr + rl + rr, l + r);
+    }
+
+    int rob(TreeNode* root) {
+        int l, r;
+        return tryRob(root, l, r);
+    }
+};
