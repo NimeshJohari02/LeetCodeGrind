a8ddb600b855d9af21aa011e8e34c2be7915e3f4
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2021-12-11 19:44:26 +0530

    Time: 4 ms (13.79%), Space: 5.9 MB (30.46%) - LeetHub

---
+++ nth-magical-number/nth-magical-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int mod = 1e9+7;
+ int nthMagicalNumber(int N, int A, int B) {
+         long lcm = A * B / __gcd(A, B), l = 2, r = 1e14, mod = 1e9 + 7;
+         while (l < r) {
+             long m = (l + r) / 2;
+             if (m / A + m / B - m / lcm < N) l = m + 1;
+             else r = m;
+         }
+         return l % mod;
+     }
+};
