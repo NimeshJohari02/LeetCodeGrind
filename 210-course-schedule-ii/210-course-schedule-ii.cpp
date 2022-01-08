ce6cb36b5a3c318e7c8423397dfe24397812c99c
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2022-01-08 10:49:34 +0530

    Time: 28 ms (25.83%), Space: 13.1 MB (96.63%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& arr) {
+        vector<int>indegree(n , 0 )  ;
+        vector<int>ans;
+				vector<int>graph[n];
+				for(auto &it : arr){
+								graph[it[1]].push_back(it[0]);
+								indegree[it[0]]++;
+				}
+				queue<int>q;
+			for(int i=0 ; i < n ; i++)
+                if(indegree[i]==0)
+                    q.push(i);
+        
+        
+				while(!q.empty()){
+								int node = q.front();
+								q.pop();
+								ans.push_back(node);
+								for(auto&it : graph[node]){
+												indegree[it]--;
+												if(indegree[it]==0)
+																q.push(it);
+								}
+				}
+		return ans.size()==n ?ans : vector<int>(); 
+    }
+};
