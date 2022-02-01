ab5db2192e660720757c4cb9506fb2679423e7da
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2022-02-01 20:49:40 +0530

    Update 1463-cherry-pickup-ii.cpp
    Add memoized Solution

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -22,3 +22,43 @@ public:
         return dp[r][c1][c2] = ans + cherries;
     }
 };
+// This is the memoized Solution
+class Solution {
+public:
+    int dp[71][71][71];
+    int memoize(int i , int j1 , int j2 , vector<vector<int>>&grid){
+        int n = grid.size();
+        int m = grid[0].size();
+        if(i<0 || j1 < 0 || j2<0 || i >= n|| j1 >=m|| j2>=m  )return -1e8;
+        if(i==n-1){
+            if(j1==j2) return grid[i][j1];
+            else return (grid[i][j1] + grid[i][j2]);
+        }
+        
+        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+        
+        int maxi = -1e8;
+        for(int r1= -1 ; r1<=1;r1++){
+            for(int r2 = -1 ; r2<=1 ; r2++){
+                int value =0;
+                if(j1==j2){
+                    value = grid[i][j1];
+                }
+                else{
+                    value =  grid[i][j1]+grid[i][j2];
+                }
+                value+=memoize(i+1 , j1+r1 , j2+r2 , grid);
+                maxi = max(maxi , value);
+            }
+        }
+        return dp[i][j1][j2] = maxi;
+    }
+    int cherryPickup(vector<vector<int>>& grid) {
+        int row = grid.size();
+        int cols = grid[0].size();
+        memset(dp , -1 ,sizeof(dp));
+        
+        return memoize(0 , 0 , cols-1 , grid);
+        
+    }
+};
