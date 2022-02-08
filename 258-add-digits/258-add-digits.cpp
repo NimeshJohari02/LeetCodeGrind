b0eb19113e92101ab3c1b55da2db6766e92a0616
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2022-02-08 12:15:53 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (75.31%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
-    int r(int n){
-        if(n<10)
-            return n;
-        int s=0;
-        while(n){
-            s += n%10; 
-            n /=10; 
-        }
-        return r(s);
+    // int r(int n){
+    //     if(n<10)
+    //         return n;
+    //     int s=0;
+    //     while(n){
+    //         s += n%10; 
+    //         n /=10; 
+    //     }
+    //     return r(s);
+    // }
+    int helper(int n) {
+  if (n <= 9)
+    return n;
+  while (1) {
+    if (n <= 9)
+      return n;
+    int temp = n;
+    int sum = 0;
+    while (temp) {
+      int ld = temp % 10;
+      temp /= 10;
+      sum += ld;
     }
+    n = sum;
+  }
+}
     int addDigits(int num) {
-        return r(num);
+        return helper(num);
     }
 };
