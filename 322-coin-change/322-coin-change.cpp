5e04336c44e8c29e7ce98df08911ab85edf43163
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2022-02-11 14:40:18 +0530

    Time: 88 ms (68.44%), Space: 14.5 MB (49.71%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -62,13 +62,26 @@ public:
         }
         return prev[target];
     }
-    
+    int singleArrayOptimization(vector<int>&coins , int amount){
+      vector<int> dp(amount+1 ,amount+1);
+        // For making an amount as 0 you need 0 coins thus the initialization condition for such cases changes 
+        dp[0]=0;
+        for(int i = 1 ; i <= amount ; i++){
+            for(int j = 0 ; j < coins.size() ; j++){
+                if(i >= coins[j] ){
+                    dp[i] = min(dp[i], dp[i-coins[j]]+1);
+                }
+            }
+        }
+        return dp[amount]== amount+1 ? -1 : dp[amount];
+    }
     int coinChange(vector<int>& coins, int amount) {
         // return rec(coins.size()-1 , coins , amount);
         // vector<vector<int>>dp(coins.size() , vector<int>(amount+1 , -1));
         // int ans =  memo(coins.size()-1 , coins , amount , dp);
         // int ans = tabulation(coins , amount);
-        int ans = spaceOptimization(coins , amount);
-        return ans == 1e9 ? -1 : ans;
+        // int ans = spaceOptimization(coins , amount);
+        // return ans == 1e9 ? -1 : ans;
+        return singleArrayOptimization(coins , amount);
     }
 };
