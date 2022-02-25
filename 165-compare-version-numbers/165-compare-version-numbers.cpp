726d015d39d186f2119b3ddcac3c50468e94f2aa
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2022-02-25 09:20:07 +0530

    Time: 3 ms (31.35%), Space: 6.2 MB (35.61%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -16,18 +16,49 @@ public:
     }
      return tokens;
     }
-    int compareVersion(string s1, string s2) {
-         vector<long long int>v1 = tokeinizingFunction(s1);
-         vector<long long int>v2 = tokeinizingFunction(s2);
-        int i=0;
-        int j=0;
-        for(auto &it : v1){
-            cout<<it<<"   ";
+    // int compareVersion(string s1, string s2) {
+    //      vector<long long int>v1 = tokeinizingFunction(s1);
+    //      vector<long long int>v2 = tokeinizingFunction(s2);
+    //     int i=0;
+    //     int j=0;
+    //     for(auto &it : v1){
+    //         cout<<it<<"   ";
+    //     }
+    //     for(int i=0 ; i < max(v1.size() , v2.size()) ; i++){
+    //         if(((i>=v1.size()) ? 0 : v1[i])>(i>=v2.size() ? 0 :v2[i]))return 1;
+    //         else if(((i>=v1.size()) ? 0 : v1[i])<(i>=v2.size() ? 0 :v2[i]))return -1;
+    //     }
+    // return 0;
+    // }
+int compareVersion(string version1, string version2) {
+    int i = 0; 
+    int j = 0;
+    int n1 = version1.size(); 
+    int n2 = version2.size();
+    
+    int num1 = 0;
+    int num2 = 0;
+    while(i<n1 || j<n2)
+    {
+        while(i<n1 && version1[i]!='.'){
+            num1 = num1*10+(version1[i]-'0');
+            i++;
         }
-        for(int i=0 ; i < max(v1.size() , v2.size()) ; i++){
-            if(((i>=v1.size()) ? 0 : v1[i])>(i>=v2.size() ? 0 :v2[i]))return 1;
-            else if(((i>=v1.size()) ? 0 : v1[i])<(i>=v2.size() ? 0 :v2[i]))return -1;
+        
+        while(j<n2 && version2[j]!='.'){
+            num2 = num2*10+(version2[j]-'0');;
+            j++;
         }
-    return 0;
+        
+        if(num1>num2) return 1;
+        else if(num1 < num2) return -1;
+        
+        num1 = 0;
+        num2 = 0;
+        i++;
+        j++;
     }
+    
+    return 0;
+}
 };
