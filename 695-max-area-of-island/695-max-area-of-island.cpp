3128024d7dcf7391b6bc8261d8eddcd9adaa2975
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2022-04-04 16:40:31 +0530

    Time: 24 ms (63.73%), Space: 23.3 MB (66.34%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,37 +1,44 @@
 class Solution {
 public:
     
-    int bfs(vector<vector<int>>&grid , int row , int col){
-        queue<pair<int , int >> q;
-        int m = grid.size();
-        int n = grid[0].size();
-        q.push({ row , col});
-        int area =1 ;
-        grid[row][col] = 1e9;
-        while(!q.empty()){
-            auto node = q.front();
-            q.pop();
-            int dx[]={1 , -1 , 0 , 0};
-            int dy[]={0 , 0 , 1 , -1};
-            for(int i=0 ; i < 4 ; i++){
-                int nx = node.first + dx[i];
-                int ny = node.second+ dy[i];
-                if(nx >=0 && ny >=0 && nx < m && ny < n && grid[nx][ny] ==1 )
-                {
-                    grid[nx][ny] = 1e9;
-                    area++;
-                    q.push({nx , ny});
-                }
-            }
+//     int bfs(vector<vector<int>>&grid , int row , int col){
+//         queue<pair<int , int >> q;
+//         int m = grid.size();
+//         int n = grid[0].size();
+//         q.push({ row , col});
+//         int area =1 ;
+//         grid[row][col] = 1e9;
+//         while(!q.empty()){
+//             auto node = q.front();
+//             q.pop();
+//             int dx[]={1 , -1 , 0 , 0};
+//             int dy[]={0 , 0 , 1 , -1};
+//             for(int i=0 ; i < 4 ; i++){
+//                 int nx = node.first + dx[i];
+//                 int ny = node.second+ dy[i];
+//                 if(nx >=0 && ny >=0 && nx < m && ny < n && grid[nx][ny] ==1 )
+//                 {
+//                     grid[nx][ny] = 1e9;
+//                     area++;
+//                     q.push({nx , ny});
+//                 }
+//             }
+//         }
+//         return area;
+//     }
+    int calculateArea(vector<vector<int>> &grid , int currRow , int currCol){
+        if(currRow >= 0 && currCol >=0 && currRow < grid.size() && currCol < grid[0].size() && grid[currRow][currCol] == 1){
+            grid[currRow][currCol] = 1e9;
+            return 1+ calculateArea(grid , currRow+1 , currCol) + calculateArea(grid , currRow-1 , currCol) + calculateArea(grid , currRow , currCol+1) + calculateArea(grid , currRow , currCol-1); 
         }
-        return area;
+        return 0 ;
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int m = grid.size() , n = grid[0].size() ;
         int ans = 0 ;
         for(int i=0 ; i < m ; i++)
             for(int j=0 ; j <n ; j++)
-                if(grid[i][j]==1 ) ans = max(ans , bfs(grid , i , j));
+                if(grid[i][j]==1 ) ans = max(ans , calculateArea(grid , i , j));
         return ans;
     }
 };
