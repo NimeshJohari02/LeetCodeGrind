eaff2d2ccca7963b2fa14ccca1052b71bad8ae74
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2022-04-05 15:42:20 +0530

    Time: 109 ms (60.25%), Space: 31.8 MB (13.27%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,5 +1,47 @@
 class Solution {
 public:
+    /* 
+    1. For Union Find we generally maintain a DSU class . And the implentation consists of a few vectors . But in our case that DSU class will not work in order to facilitate the storing of negative numbers . 
+    */
+    int find(vector<int>& ds,int i){
+        return ds[i]<0?i:ds[i]=find(ds,ds[i]);
+    }
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.empty())
+            return 0;
+        int seq=0;
+        vector<int> ds(nums.size(),-1);
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();++i){
+            int n=nums[i];
+            if(m.count(n))
+                continue;
+            m[n]=i;
+            if(m.count(n-1)){
+                //union
+                int j=find(ds,m[n]);
+                int k=find(ds,m[n-1]);
+                ds[j]+=ds[k];
+                ds[k]=j;
+            }           
+            if(m.count(n+1)){
+                //union
+                int j=find(ds,m[n]);
+                int k=find(ds,m[n+1]);
+                ds[j]+=ds[k];
+                ds[k]=j;
+            }
+            seq=max(seq,abs(ds[find(ds,m[n])]));
+        }
+        return seq;
+    }
+};
+
+/*
+
+HashMap Approach 
+class Solution {
+public:
     int longestConsecutive(vector<int>& nums) {
      unordered_map<int , int> mp;
         if(nums.size() ==0) return 0;
@@ -20,3 +62,6 @@ public:
         return ls;
     }
 };
+
+
+*/
