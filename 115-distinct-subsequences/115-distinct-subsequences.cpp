a29220c62c75b200482163edeef15df5ef36ed5a
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2022-04-18 18:54:00 +0530

    Time: 49 ms (50.24%), Space: 30.2 MB (5.96%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -9,9 +9,27 @@ public:
         }
         return dp[idx1][idx2] = helper(idx1-1 , idx2 , s1 , s2 , dp);
     }
+    int tabulation(string &s1 , string &s2){
+        long long int m = s1.size() , n = s2.size();
+        vector<vector<double>> dp(m+1, vector<double>(n+1 , 0));
+        for(int i=0 ; i <= m ; i++)
+            dp[i][0] =1 ;
+        for(int i=1 ; i <= m ; i++){
+            for(int j=1 ; j <= n ; j++){
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j]  = dp[i-1][j-1]+dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return (int)dp[m][n];
+    }
     int numDistinct(string s, string t) {
         int m = s.size() , n = t.size();
-        vector<vector<int>> dp(m , vector<int>(n , -1));
-        return helper(m-1 ,n-1 , s , t , dp);
+        // vector<vector<int>> dp(m , vector<int>(n , -1));
+        // return helper(m-1 ,n-1 , s , t , dp);
+        return tabulation(s , t);
     }
 };
