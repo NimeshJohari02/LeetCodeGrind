7bb225206e37af79677e97611fa8d54e05e8f068
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2022-04-18 19:08:46 +0530

    Time: 35 ms (75.67%), Space: 6.5 MB (90.60%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -26,10 +26,28 @@ public:
         }
         return (int)dp[m][n];
     }
+    int spaceOptimized(string &s1 , string &s2){
+         int m = s1.size() , n = s2.size();
+        vector<double> prev(n+1 , 0) , curr(n+1 , 0);
+        prev[0]=curr[0] =1 ;
+        
+        for(int i=1 ; i <= m ; i++){
+            for(int j=1 ; j <= n ; j++){
+                if(s1[i-1] == s2[j-1]){
+                    curr[j]  = prev[j-1]+prev[j];
+                }
+                else{
+                    curr[j] = prev[j];
+                }
+            }
+            prev = curr;
+        }
+        return (int)prev[n];
+    }
     int numDistinct(string s, string t) {
-        int m = s.size() , n = t.size();
+        // int m = s.size() , n = t.size();
         // vector<vector<int>> dp(m , vector<int>(n , -1));
         // return helper(m-1 ,n-1 , s , t , dp);
-        return tabulation(s , t);
+        return spaceOptimized(s , t);
     }
 };
