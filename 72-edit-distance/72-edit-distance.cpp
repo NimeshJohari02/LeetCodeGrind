3687a68620d75d014ce320abe1e095db88ebe207
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2022-04-18 21:00:19 +0530

    Time: 11 ms (78.68%), Space: 11 MB (15.26%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -12,9 +12,31 @@ public:
             int replace =  helper(idx1 -1 , idx2 -1 , s, t, dp);
             return dp[idx1][idx2] =1 +  min(min(insert , deleteElement) , replace);
     }
+    
+    
+    int tabulation(string &s1 , string &s2){
+        int m = s1.size() , n = s2.size() ;
+        vector<vector<long long int>> dp(m+1 , vector<long long int>(n+1 , 0));
+        for(int i=0;i<=m ;i++)
+            dp[i][0] = i;
+        for(int i=0;i<=n ;i++)
+            dp[0][i] = i;
+        for(int i=1 ; i <=m ; i++){
+            for(int j=1 ; j<= n ; j++){
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 1+ min(min(dp[i-1][j-1], dp[i-1][j]), dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
     int minDistance(string word1, string word2) {
-        int m = word1.size() , n = word2.size() ;
-        vector<vector<int>>dp(m , vector<int>(n , -1));
-        return helper(m-1 , n-1 , word1 , word2 , dp);
+        // int m = word1.size() , n = word2.size() ;
+        // vector<vector<int>>dp(m , vector<int>(n , -1));
+        // return helper(m-1 , n-1 , word1 , word2 , dp);
+        return tabulation(word1 , word2);
     }
 };
