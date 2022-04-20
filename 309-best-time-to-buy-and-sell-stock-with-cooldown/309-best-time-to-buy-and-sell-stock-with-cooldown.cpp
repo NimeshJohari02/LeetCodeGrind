4ccd8237dc621b3d3f68954468cc5aeb50634914
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2022-04-20 12:16:40 +0530

    Time: 5 ms (57.41%), Space: 11.8 MB (37.97%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -4,12 +4,13 @@ public:
     int tabulation(vector<int>&prices ){
         int n =prices.size() ;
         vector<vector<int>>dp(n+2 , vector<int>(2 , 0));
-        for(int i= n-1 ; i >=0 ;i--)
-            for(int b=0 ; b<=1 ; b++)
-                if(b)
-                    dp[i][b] = max(-prices[i] + dp[i+1][0] , dp[i+1][1]);
-                else
-                    dp[i][b] = max(prices[i] + dp[i+2][1] , dp[i+1][0]);
+        for(int i= n-1 ; i >=0 ;i--){
+            // for(int b=0 ; b<=1 ; b++)
+                // if(b)
+                    dp[i][1] = max(-prices[i] + dp[i+1][0] , dp[i+1][1]);
+                // else
+                    dp[i][0] = max(prices[i] + dp[i+2][1] , dp[i+1][0]);
+        }
     return dp[0][1];
     }
     int maxProfit(vector<int>& prices) {
