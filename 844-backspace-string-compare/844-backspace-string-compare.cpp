c7c16ff819b054b4affcd3981a1faef35aade368
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2022-05-01 11:02:46 +0530

    Time: 2 ms (47.87%), Space: 6.2 MB (68.92%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -16,7 +16,43 @@ public:
         }
         return ans;
     }
+
     bool backspaceCompare(string s, string t) {
-        return  gs(s)==gs(t);
+ int i = s.size() - 1; 
+        int j = t.size() - 1; 
+
+        int t_bk = 0; 
+        int s_bk = 0;
+
+        while(i >= 0 or j >= 0) {
+            while(i>=0) {
+                if(s[i]=='#') {
+                    s_bk += 1; 
+                    i -= 1; 
+                }
+                else if(s_bk) {
+                    s_bk -= 1; 
+                    i -= 1; 
+                }
+                else break;
+            }
+            while(j>=0) {
+                if(t[j]=='#') {
+                    t_bk += 1; 
+                    j -= 1; 
+                }
+                else if(t_bk) {
+                    t_bk -= 1; 
+                    j -= 1; 
+                }
+                else break;
+            }
+
+            if(i>=0 and j>=0 and !t_bk and !s_bk and s[i] != t[j]) return false; 
+            if ((i >= 0) != (j >= 0))return false;
+            i -= 1; 
+            j -= 1;
+        }
+        return true; 
     }
 };
