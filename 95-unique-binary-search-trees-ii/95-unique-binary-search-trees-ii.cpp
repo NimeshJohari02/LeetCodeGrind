f1f1acf66172415ba3be2d0c94515cb70dc9238a
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2022-06-12 23:34:56 +0530

    Time: 8 ms (99.22%), Space: 12.2 MB (97.84%) - LeetHub

--- 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.cpp
+++ 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.cpp
@@ -11,25 +11,25 @@
  */
 class Solution {
 public:
-    
-    vector<TreeNode*>helper(int start , int end){
+    vector<TreeNode*>helper(int start , int end ,map<pair<int,int> , vector<TreeNode*>>&dp){
         vector<TreeNode*> v;
         if(start > end) {
             v.push_back(NULL);
             return v;
         }
+        if(dp.find({start , end}) != dp.end()) return dp[{start , end}];
         for(int root = start ; root <= end ; root++){
-            auto left = helper(start , root-1);
-            auto right = helper(root+1 , end);
+            auto left = helper(start , root-1 , dp);
+            auto right = helper(root+1 , end , dp);
             for(auto &l : left)
                 for(auto &r : right)
                     v.push_back(new TreeNode(root , l , r));
         }
-        return v;
+        return dp[{start , end}]  = v;
     }
     vector<TreeNode*> generateTrees(int n) {
         if(n==0) return {};
-        
-        return helper(1 , n);
+       map<pair<int,int> , vector<TreeNode*>> dp;
+        return helper(1 , n , dp);
     }
 };
