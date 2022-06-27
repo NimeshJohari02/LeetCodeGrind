a75c2dc0af3be1123a021ccff8f2640d41102cf7
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2022-06-27 12:53:24 +0530

    Time: 93 ms (42.49%), Space: 42.5 MB (48.00%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,13 +1,39 @@
+// class Solution {
+// public:
+//   int maxScore(vector<int>& nums, int k) {
+//         int total = accumulate(nums.begin() , nums.end() , 0);
+//         if(k==nums.size())return total;
+//         int cs = accumulate(nums.begin() , nums.end()-k , 0);
+//         int ans = total - cs;
+//         for(int i=0 , j = nums.size() - k; j < nums.size() ; j++ , i++){
+//             ans=max(ans ,  total - (cs += nums[j]-nums[i]));
+//         }
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
     int maxScore(vector<int>& nums, int k) {
+        int i =0 , j =0 , cs = 0 ;
         int total = accumulate(nums.begin() , nums.end() , 0);
         if(k==nums.size())return total;
-        int cs = accumulate(nums.begin() , nums.end()-k , 0);
-        int ans = total - cs;
-        for(int i=0 , j = nums.size() - k; j < nums.size() ; j++ , i++){
-            ans=max(ans ,  total - (cs += nums[j]-nums[i]));
+        int minWindowSum = INT_MAX;
+        k=nums.size()-k;
+        for(int j=0 ; j < nums.size() ; j++){
+        cs+=nums[j];
+             if(j-i+1 > k){
+                cs-=nums[i++];
+                minWindowSum = min(minWindowSum , cs);
+            }
+            if(j-i+1 == k){
+                minWindowSum = min(minWindowSum , cs);
+            }
+           
         }
-        return ans;
+        cout<<minWindowSum<<"\n Total = "<<total<<"\n";
+        return total-minWindowSum;
     }
 };
