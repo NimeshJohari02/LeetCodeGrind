72466a8d54ea847a201333c6f16373dc85f2eb87
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2022-07-09 20:24:33 +0530

    Time: 445 ms (22.40%), Space: 80.7 MB (70.83%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -35,11 +35,26 @@ public:
         }
         return dp.back();
     }
+    int sol(vector<int>&nums ,int k){
+        int n = nums.size();
+        vector<int>dp(n , 0);
+        deque<int> q;
+        q.push_back(0);
+        dp[0] = nums[0];
+        for(int i= 1; i <= n-1 ; i++){
+            if(q.front() < i-k) q.pop_front();
+            dp[i] = nums[i] + dp[q.front()];
+            while(!q.empty() && dp[i] >= dp[q.back()]) q.pop_back();
+            q.push_back(i);
+        }
+        return dp.back();
+    }
     int maxResult(vector<int>& nums, int k) {
         // int n = nums.size() ;
         // vector<int>dp(n+1 , -1 );
         // return helper(0 , nums , k , dp);
-        return solUsingMultiSet(nums , k );
+        // return solUsingMultiSet(nums , k );
+        return sol(nums , k);
     }
 };
 
