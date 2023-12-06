25114b0f9e7686d559841af42f0ab97136d54437
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2023-12-07 01:18:24 +0530

    Time: 369 ms (12.88%), Space: 18.6 MB (5.09%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-
-    int jump(vector<int>& nums) {
-        int ans = 0 ,  end = 0 , maxReach = 0 ;
-        //Since it is already said that we can reach the last element we need not consider the jump last element provides We need to reach last element not jump from last element also. So reduce the size of the array by 1 . 
+    int greedySolution(vector<int>&nums){
+                int ans = 0 ,  end = 0 , maxReach = 0 ;
+        // Since it is already said that we can reach the last element we need not consider            the jump last element provides We need to reach last element not jump from last              element   
+        // also. So reduce the size of the array by 1 . 
         for(int i=0 ; i < nums.size()-1; i++){
             maxReach = max(maxReach , i + nums[i]);
             if(i==end){
@@ -12,5 +12,25 @@ public:
             }
         }
         return ans ; 
+    }
+    int recursive(int idx , vector<int>&nums){
+        if(idx>=nums.size()-1)return 0;
+        int minJumps = 1e5;
+        for(int j = 1 ; j <= nums[idx];j++)
+            minJumps = min(minJumps , 1+recursive(idx+j,nums));
+        return minJumps;
+    }
+    int memoization(int idx , vector<int>&nums , vector<int>&dp){
+        if(idx>=nums.size()-1) return 0 ;
+        if(dp[idx]!= 1e5)return dp[idx];
+        int minJumps = 1e5 ;
+        for(int j=1 ; j <=nums[idx];j++)
+            minJumps = min(minJumps , 1+memoization(idx+j ,nums , dp));
+        return dp[idx] = minJumps ;
+    }
+    int jump(vector<int>& nums) {
+        // return recursive(0 , nums);
+        vector<int>dp(nums.size() , 1e5);
+        return memoization(0 , nums , dp);
     }
 };
