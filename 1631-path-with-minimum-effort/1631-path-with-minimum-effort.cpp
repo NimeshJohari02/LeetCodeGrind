e29de92802cb4b0b57c061d9473e588d66dde04f
Nimesh Johari <62381277+NimeshJohari02@users.noreply.github.com>
2024-05-06 20:43:22 +0530

    Time: 83 ms (82.35%), Space: 23.5 MB (86.81%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,3 +1,4 @@
+
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
@@ -6,31 +7,31 @@ public:
     // Generally this statement is for binary search in case in a 1d 
     // Since this is a 2d grid we can use djikstra
         vector<vector<int>>minCostGrid(n , vector<int>(m , 1e9));
-        priority_queue<vector<int> , vector<vector<int>> , greater<vector<int>>> pq ;
+        // priority_queue<vector<int> , vector<vector<int>> , greater<vector<int>>> pq ;
+        priority_queue<pair<int , pair<int , int>> , vector<pair<int , pair<int , int>>> , greater<pair<int , pair<int , int>>>>pq;
         // Difference  x Coord , y Coord
-        pq.push({0 , 0 , 0 });
+        pq.push({0 , {0 , 0}});
         minCostGrid[0][0]=0 ;
         while(!pq.empty()){
             auto vec = pq.top();
             pq.pop();
-            int currentDifference = vec[0];
-            int x = vec[1];
-            int y = vec[2];
+            int currentDifference = vec.first;
+            int x = vec.second.first;
+            int y = vec.second.second;
             if(x==n-1 && y==m-1 )return currentDifference;
             int dx[] = {0 , 0 , 1 , -1};
             int dy[] = {1 ,-1 , 0 , 0 };
             for(int k =0 ; k < 4 ; k++){
-                int nx= x+dx[k];
-                int ny =y+dy[k];
+                int nx = x+dx[k];
+                int ny = y+dy[k];
                 if(nx < n && nx >= 0 && ny < m && ny >= 0){
                     int newEffort = max(currentDifference , abs(heights[nx][ny] - heights[x][y]));
                     if(newEffort < minCostGrid[nx][ny] ){
                         minCostGrid[nx][ny]= newEffort ;
-                        pq.push({newEffort , nx , ny});
+                        pq.push({newEffort ,{nx , ny}});
                     }
                 }
-            }
-            
+            }   
         }
         return 0;
     }
